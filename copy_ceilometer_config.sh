d5bf4572776ea45a6a1e931a8b5e2aee2cc7ecfc
hswayne77 <hswayne77@gmail.com>
2013-04-29 02:50:34 +0800

    Update copy_ceilometer_config.sh

--- copy_ceilometer_config.sh
+++ copy_ceilometer_config.sh
@@ -5,8 +5,8 @@ set -x
 CEILOMETER_CONFIG_PATH=/etc/ceilometer
 if [ -e ${CEILOMETER_CONFIG_PATH} ];then
     rm -rf ${CEILOMETER_CONFIG_PATH}
-    mkdir -p /etc/ceilometer
 fi
+mkdir -p /etc/ceilometer
 cp etc/ceilometer/*.json /etc/ceilometer
 cp etc/ceilometer/*.yaml /etc/ceilometer
 cp etc/ceilometer/ceilometer.conf.sample /etc/ceilometer/ceilometer.conf
