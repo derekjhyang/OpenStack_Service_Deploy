ba27b7c472a8d9d9fae13908aad1cf82ff39cd76
Derrick Yang <hswayne77@gmail.com>
2014-12-31 13:26:48 +0800

    Update install_keystone.sh

--- install_keystone.sh
+++ install_keystone.sh
@@ -11,7 +11,18 @@ DATABASE=keystone
 
 SERVICE_USER=keystone
 SERVICE_PASSWORD=keystone
+SERVICE_PUBLIC_PORT=5000
+SERVICE_INTERNAL_PORT=5000
+SERVICE_ADMIN_PORT=35357
+API_VERSION=V2.0
 SERVICE_CONFIG_PATH=/etc/${SERVICE_USER}/${SERVICE_USER}.conf
+
+CONTROLLER_IP=localhost
+ENDPOINT_PUBLIC_URL=http://${CONTROLLER_IP}:${SERVICE_PUBLIC_PORT}/${API_VERSION}
+ENDPOINT_INTERNAL_URL=http://${CONTROLLER_IP}:${SERVICE_INTERNAL_PORT}/${API_VERSION}
+ENDPOINT_ADMIN_URL=http://${CONTROLLER_IP}:${SERVICE_ADMIN_PORT}/${API_VERSION}
+ENDPOINT_REGION=RegionOne
+
 
 # ===============
 # Create database
@@ -31,6 +42,7 @@ apt-get install -y keystone python-keystoneclient
 # ======================
 # Update Identity Config
 # ======================
+cp ${SERVICE_CONFIG_PATH} ${SERVICE_CONFIG_PATH}.sample
 # generate admin token
 ADMIN_TOKEN=$(openssl rand -hex $((${RANDOM}/100)))
 if [ -f ${SERVICE_CONFIG_PATH} ];then
@@ -42,12 +54,26 @@ if [ -f ${SERVICE_CONFIG_PATH} ];then
     DRIVER=keystone.token.persistence.backend.sql.Token
     sed -i -r "s/^(#? *)(provider)( *= *)(.*)/provider = ${PROVIDER}/g" ${SERVICE_CONFIG_PATH}
     sed -i -r "s/^(#? *)(driver)( *= *)(.*)/driver = ${DRIVER}/g" ${SERVICE_CONFIG_PATH}
+
         
 fi
 
-    
+
 # sync the 'keystone' database
 keystone-manage db_sync
 
 # restart keystone
 service keystone restart
+
+sleep 5
+
+# Create the service entity for the identity service:
+keystone service-create --name ${SERVICE_USER} --type identity \
+--description "OpenStack Identity"
+
+keystone endpoint-create \
+--service-id $(keystone service-list | awk '/identity/{print $2}') \
+--publicurl ${ENDPOINT_PUBLIC_URL} \
+--internalurl ${ENDPOINT_INTERNAL_URL} \
+--adminurl ${ENDPOINT_ADMIN_URL} \
+--region ${ENDPOINT_REGION}
